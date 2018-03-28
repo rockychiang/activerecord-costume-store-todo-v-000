73d259644fedd6752b3f867934215f5031e194d4
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-28 22:00:27 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -4,7 +4,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
     create_table :costumes do |t|
       t.string :name
       t.float :price
-      t.integer :size
+      t.string :size
       t.text :image_url
     end
   end
